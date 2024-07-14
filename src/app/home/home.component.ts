412cfdcea39a782a7563fd9ea74562c0c8440f23
RohitSIT <rohit@spareit.co.in>
2024-07-14 17:06:50 +0530

    Updated Images.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,10 +7,10 @@ import { Component, OnInit, AfterViewInit, ViewChildren, QueryList, ElementRef, 
 })
 export class HomeComponent implements AfterViewInit {
   videos = [
-    { src: 'assets/video/MODEL_1.mp4', id: 'MODEL_1', imgHeight: "270", name: "Dog Home", subtitle: "New and used Dog Houses for sale in Pune, India on Facebook Marketplace. Find great deals and sell your items for free." },
-    { src: 'assets/video/MODEL_4.mp4', id: 'MODEL_4', imgHeight: "300", name: "Dog Crate", subtitle: "Midwest Homes for Pets Dog Crates Cage Kennel 42 Inch - Spacious And Secure Crate For Large Adult Dogs (107X74X84 Cm) - Robust Metal Construction - Portable." },
-    { src: 'assets/video/MODEL_2.mp4', id: 'MODEL_2', imgHeight: "330", name: "Puppy Play Pen", subtitle: "RvPaws Foldable Metal Pet Dog Exercise Fence Pen with Gate, Foldable Metal Dog Fence, Indoor/Outdoor Enclosure with Gate for Dogs, Color Silver Black," },
-    { src: 'assets/video/MODEL_3.mp4', id: 'MODEL_3', imgHeight: "300", name: "Dog Gate", subtitle: "WoodCraftia Wooden Freestanding Foldable Safety Gate - Portable Pet Fence for Doorways, Stairs for Kids and Dogs - L-54 x H-22 Inch, 3-Panel (X Design - Brown)." },
+    { images: ["assets/img/dog_house_1.jpeg", "assets/img/dog_house_2.jpeg", "assets/img/dog_house_3.jpeg", "assets/img/dog_house_4.jpeg"], src: 'assets/video/MODEL_1.mp4', id: 'MODEL_1', name: "Dog Home", subtitle: "New and used Dog Houses for sale in Pune, India on Facebook Marketplace. Find great deals and sell your items for free." },
+    { images: ["assets/img/dog_crate_1.jpeg", "assets/img/dog_crate_2.jpeg", "assets/img/dog_crate_3.jpeg", "assets/img/dog_crate_4.jpeg"], src: 'assets/video/MODEL_4.mp4', id: 'MODEL_4', name: "Dog Crate", subtitle: "Midwest Homes for Pets Dog Crates Cage Kennel 42 Inch - Spacious And Secure Crate For Large Adult Dogs (107X74X84 Cm) - Robust Metal Construction - Portable." },
+    { images: ["assets/img/play_pen_1.jpeg", "assets/img/play_pen_2.jpeg", "assets/img/play_pen_3.jpeg", "assets/img/play_pen_4.jpeg"], src: 'assets/video/MODEL_2.mp4', id: 'MODEL_2', name: "Puppy Play Pen", subtitle: "RvPaws Foldable Metal Pet Dog Exercise Fence Pen with Gate, Foldable Metal Dog Fence, Indoor/Outdoor Enclosure with Gate for Dogs, Color Silver Black," },
+    { images: ["assets/img/dog_gate_1.jpeg", "assets/img/dog_gate_2.jpeg", "assets/img/dog_gate_3.jpeg", "assets/img/dog_gate_4.jpeg"], src: 'assets/video/MODEL_3.mp4', id: 'MODEL_3', name: "Dog Gate", subtitle: "WoodCraftia Wooden Freestanding Foldable Safety Gate - Portable Pet Fence for Doorways, Stairs for Kids and Dogs - L-54 x H-22 Inch, 3-Panel (X Design - Brown)." },
   ];
 
   @ViewChildren('reel')
